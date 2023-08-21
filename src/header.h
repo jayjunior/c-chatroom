ab6a80d95c40b5fa9da5ee0d564ce1322c495f0b
JayJunor <Mvojunior237@gmail.com>
2023-08-21 20:43:41 +0200

    modified standard name , response and msg size in header.h to include null byte too

--- src/header.h
+++ src/header.h
@@ -2,9 +2,9 @@
 #include <stdatomic.h>
 
 #define NUMBER_PARTICIPANTS 8
-#define RESPONSE_SIZE 130
-#define MSG_SIZE 101 // \n character considered
-#define NAME_SIZE 21 // \n character considered
+#define RESPONSE_SIZE 132
+#define MSG_SIZE 102 // \n and null byte character considered
+#define NAME_SIZE 22 // \n character considered
 
 typedef struct client_info
 {
